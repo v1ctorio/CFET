7107a535cc49c0782ee0a0adbebe7f5d929cd255
Vic <74506415+v1ctorio@users.noreply.github.com>
2024-08-01 11:07:25 +0200

    login screen

--- app/(tabs)/login.tsx
+++ app/(tabs)/login.tsx
@@ -1,29 +1,59 @@
-import Ionicons from '@expo/vector-icons/Ionicons';
-import { StyleSheet, Image, Platform, Text, TextInput } from 'react-native';
+import Ionicons from "@expo/vector-icons/Ionicons";
+import {
+  StyleSheet,
+  Image,
+  Platform,
+  Text,
+  TextInput,
+  Button,
+  Linking,
+} from "react-native";
 
-import { Collapsible } from '@/components/Collapsible';
-import { ExternalLink } from '@/components/ExternalLink';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { Collapsible } from "@/components/Collapsible";
+import { ExternalLink } from "@/components/ExternalLink";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
 
 export default function LogInScreen() {
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
-      headerImage={<Ionicons size={310} name="code-slash" style={styles.headerImage} />}>
+      headerBackgroundColor={{ light: "#D0D0D0", dark: "#353636" }}
+      headerImage={
+        <Ionicons size={310} name="code-slash" style={styles.headerImage} />
+      }
+    >
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Log in with cloudflare</ThemedText>
       </ThemedView>
-      <ThemedText>Use your cloudflare API key to manage your emails from the app.</ThemedText>
-      <ThemedText
-      type='subtitle'
-      >API key</ThemedText>
-      <TextInput>
-  <Text>
-    <Text style={{ fontFamily:"monospace" }}>API key here</Text>
-  </Text>
-</TextInput> 
+      <ThemedText>
+        Use your cloudflare API key to manage your emails from the app.
+        </ThemedText>
+
+              <Button
+                title="Get API key"
+                color={Platform.OS === "ios" ? "#007AFF" : "#3F51B5"}
+                onPress={() => {
+                  Linking.openURL("https://dash.cloudflare.com/profile/api-tokens");
+                }}
+              ></Button>
+
+        <ThemedText type="subtitle">Email</ThemedText>
+        <TextInput
+            
+            placeholder="test@example.com"></TextInput>
+
+      <ThemedText type="subtitle">API key</ThemedText>
+      <TextInput
+        style={{ fontFamily: "monospace" }}
+        placeholder="r:e8h:79f93247fh7:8ckm32ro;;:xxaqsa2"
+      ></TextInput>
+
+      <ThemedText type="subtitle">Account ID</ThemedText>
+      <TextInput
+        style={{ fontFamily: "monospace" }}
+        placeholder="1234567890"
+      ></TextInput>
 
     </ParallaxScrollView>
   );
@@ -31,13 +61,13 @@ export default function LogInScreen() {
 
 const styles = StyleSheet.create({
   headerImage: {
-    color: '#808080',
+    color: "#808080",
     bottom: -90,
     left: -35,
-    position: 'absolute',
+    position: "absolute",
   },
   titleContainer: {
-    flexDirection: 'row',
+    flexDirection: "row",
     gap: 8,
   },
 });
