77231b6cba5061c18e31fff2dc59b91021b01c84
Vic <74506415+v1ctorio@users.noreply.github.com>
2024-08-02 14:47:38 +0200

    Manage routes screen its pretty

--- app/src/main/java/com/nosesisaid/cfer/management/ManageRoutesScreen.kt
+++ app/src/main/java/com/nosesisaid/cfer/management/ManageRoutesScreen.kt
@@ -1,6 +1,7 @@
 package com.nosesisaid.cfer.management
 
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -14,6 +15,7 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
@@ -21,6 +23,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
@@ -28,13 +31,22 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.unit.sp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Preview(showBackground = true)
 @Composable
 fun ManageRoutesScreen() {
+    var caroutes_is_checked by remember { mutableStateOf(false) }
     Scaffold (
         topBar = { TopAppBar(
             title = { Text(text = "Routes") },
@@ -60,10 +72,22 @@ fun ManageRoutesScreen() {
                 )
             {
                 Card(
-                    modifier = Modifier.fillMaxWidth().padding(vertical = 10.dp), colors = CardDefaults.cardColors(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 10.dp), colors = CardDefaults.cardColors(
                         containerColor = MaterialTheme.colorScheme.primaryContainer,
                     )) {
-                    Text(text = "Catch-all route", modifier = Modifier.padding(20.dp))
+                    Row (
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Start,
+                        verticalAlignment = Alignment.CenterVertically
+                    ){
+                        Text(text = "Catch-all route", modifier = Modifier.padding(20.dp))
+                        Spacer(modifier = Modifier.weight(1f))
+                        Switch(checked = caroutes_is_checked, onCheckedChange = { caroutes_is_checked = it }, modifier = Modifier
+                            .scale(0.7f)
+                            .padding(10.dp))
+                    }
                 }
 
                 Card(modifier = Modifier.fillMaxWidth(), colors = CardDefaults.cardColors(
@@ -74,14 +98,19 @@ fun ManageRoutesScreen() {
                     ) {
                         Text("Alias",modifier= Modifier
                             .padding(16.dp)
-                            .width(70.dp),
+                            .width(60.dp),
                             textAlign = TextAlign.Center
                         )
                         Text("State",modifier= Modifier
                             .padding(16.dp))
                         Text(text = "Action", modifier=Modifier.padding(16.dp))
-                        Text("Delete",modifier= Modifier
-                            .padding(16.dp))
+                        Text("Target",modifier= Modifier
+                            .padding(16.dp)
+                            .width(160.dp),
+                            textAlign = TextAlign.Center
+                        )
+
+
                     }
                 }
                 Spacer(modifier = Modifier.height(16.dp))
@@ -89,20 +118,24 @@ fun ManageRoutesScreen() {
                     modifier = Modifier.fillMaxWidth(),
                     shape = RoundedCornerShape(20.dp)
                 ) {
-                    Row {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
 
-                        Text("chatskdfasfadsfads@nosesisaid.com",
+                    ) {
+
+                        Text("vic",
+
                             Modifier
                                 .padding(16.dp)
-                                .width(160.dp), maxLines = 1)
-                        Text(text = "Verified",
-                            Modifier
-                                .padding(16.dp)
-                                .width(50.dp)
+                                .width(70.dp), maxLines = 1,textAlign = TextAlign.Center)
+                        Checkbox(checked = true, onCheckedChange ={/*TODO*/ },)
+                        Text(text ="Forward", modifier = Modifier.padding(16.dp).width(60.dp), fontSize = 12.sp, textAlign = TextAlign.Center)
+                        Text("hello@chat.hi",modifier= Modifier
+                            .padding(16.dp)
+                            .width(160.dp),
+                            textAlign = TextAlign.Center,
+                            fontSize = 12.sp
                         )
-                        IconButton(onClick = { /*TODO*/ }, modifier = Modifier.width(90.dp)) {
-                            Icon(imageVector = Icons.Filled.Clear, contentDescription ="Delte email" )
-                        }
                     }
                 }
                 ElevatedCard(
@@ -111,7 +144,10 @@ fun ManageRoutesScreen() {
                         .padding(top = 10.dp),
                     shape = RoundedCornerShape(20.dp)
                 ) {
-                    Row {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+
+                    ) {
 
                         Text("hichat@nosesisaid.com",
                             Modifier
