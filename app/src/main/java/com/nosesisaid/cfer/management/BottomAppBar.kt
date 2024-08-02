25ee491dc4353d64d653be81623157716e63b1fe
Vic <74506415+v1ctorio@users.noreply.github.com>
2024-08-02 10:39:07 +0200

    add navigation bar like the component itself

--- app/src/main/java/com/nosesisaid/cfer/management/BottomAppBar.kt
+++ app/src/main/java/com/nosesisaid/cfer/management/BottomAppBar.kt
@@ -1,9 +1,23 @@
 package com.nosesisaid.cfer.management
 
-import androidx.compose.material3.BottomAppBar
 
-fun CFERBottomAppBar() {
-    BottomAppBar {
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
-    }
+
+@Preview
+@Composable
+fun CFERNavigationBar() {
+
+         NavigationBar {
+             NavigationBarItem(selected = false, onClick = { /*TODO*/ }, icon = { Icon(Icons.Filled.Search, contentDescription = "Routes managment") })
+            NavigationBarItem(selected = true, onClick = { /*TODO*/ }, icon = { Icon(Icons.Filled.Email, contentDescription = "Email tab") })
+         }
+
 }
