49ce043868f8a619bf889b97d72e7366ca64477e
Vic <74506415+v1ctorio@users.noreply.github.com>
2024-08-09 20:16:00 +0200

    Set version to  0.9.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 32
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0"
+        versionName = "0.9.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
